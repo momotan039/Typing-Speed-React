0c646b67f5488fe2ef8d3fc0f0e30383729ad624
momotan <momotaha039@gmail.com>
2023-03-06 16:02:02 +0200

    create the fuctionality of sgin in page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,16 @@
 import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
-import User from "../Utils/User.mjs";
+import useLocalStorage from "../Custome Hooks/useLocalStorage";
+import Spinner from "../Spinner/Spinner";
+import { saveUserToSorage } from "../Utils/LocalStorage.mjs";
 
 export default function Login() {
     const app=useContext(AppContext)
     const [errors, setErrors] = useState({});
-    const formData={}
-    const loginUser=()=>{
+    const [showSpinner,setShowSpinner]=useState(false)
+    const formData=useRef()
+    const  loginUser=async()=>{
         event.preventDefault()
         const validationErrors = {};
        
@@ -21,13 +24,20 @@ export default function Login() {
           // check if there are any validation errors
           if (Object.keys(validationErrors).length > 0) {
               setErrors(validationErrors);
-            return;
+              return;
           }
-
-        app.login(formData)
+        setShowSpinner(true)
+        await app.login(formData).then((res)=>{
+          saveUserToSorage(res)
+          setShowSpinner(false)
+        }).catch((err)=>{
+          debugger
+          setErrors({user:err})
+        })
+        setShowSpinner(false)
     }
 
-    const changeUser=(event)=>{
+    const changeForm=(event)=>{
         const input=event.target
         formData[input.name]=input.value
     }
@@ -39,14 +49,17 @@ export default function Login() {
         </div>
         <div className="content">
             <form action="">
+            {
+          errors.user&&<div className="error jello">{errors.user}</div>
+        }
             <div className="row">
-                <h3>Email</h3>
-                <input required name="email" onChange={changeUser} type="email"/>
+                <h3>Email:</h3>
+                <input required name="email" onChange={changeForm} type="email" value={formData.email}/>
             </div>
             {errors.email && <div className="jello error">{errors.email}</div>}
             <div className="row">
-                <h3>Password</h3>
-                <input required name="password" onChange={changeUser} type="password"/>
+                <h3>Password:</h3>
+                <input required name="password" onChange={changeForm} type="password"/>
             </div>
             {errors.password && <div className="jello error">{errors.password}</div>}
             <div className="btns">
@@ -55,6 +68,9 @@ export default function Login() {
             </div>
             </form>
         </div>
+        {
+          showSpinner&&<Spinner/>
+        }
     </div>
   )
 }
