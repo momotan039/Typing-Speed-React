088caab973b617551e28436c8cd8f936447f7140
momotan <momotaha039@gmail.com>
2023-03-06 16:05:13 +0200

    create the fuctionality of sgin up page

--- src/components/Signup/SignUp.jsx
+++ src/components/Signup/SignUp.jsx
@@ -1,29 +1,88 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../../App";
+import Spinner from "../Spinner/Spinner";
+import { saveUserToSorage } from "../Utils/LocalStorage.mjs";
 
 export default function SignUp() {
+    const app=useContext(AppContext)
+    const nav=useNavigate()
+    const [errors, setErrors] = useState({});
+    const [showSpinner,setShowSpinner]=useState(false)
+    const formData={}
+
+    const changeForm=(event)=>{
+        const input=event.target
+        formData[input.name]=input.value
+    }
+
+    const signUpUser=async ()=>{
+        event.preventDefault()
+        const validationErrors = {};
+        if (!formData.name) {
+            validationErrors.name = "Name is required";
+          }
+
+          if (!formData.email) {
+            validationErrors.email = "Email is required";
+          }
+          if (!formData.password) {
+            validationErrors.password = "Password is required";
+          }
+                
+          // check if there are any validation errors
+          if (Object.keys(validationErrors).length > 0) {
+              setErrors(validationErrors);
+              return;
+          }
+
+       setShowSpinner(true)
+        await app.signUp(formData).then((res)=>{
+          saveUserToSorage(res)
+          app.login(res)
+        }).catch((err)=>{
+          setErrors({user:err})
+        })
+
+        setTimeout(() => {
+          nav('/')
+        setShowSpinner(false)
+        }, 2000);
+    }
   return (
     <div className="card jello">
         <div className="head">
         <h1>Sign up</h1>
         </div>
         <div className="content">
+            <form action="">
+            {
+          errors.user&&<div className="error jello">{errors.user}</div>
+        }
             <div className="row">
-                <h3>UserName</h3>
-                <input type="text"/>
+                <h3>Name:</h3>
+                <input required name="name" onChange={changeForm} type="email"/>
             </div>
+            {errors.name && <div className="jello error">{errors.name}</div>}
             <div className="row">
-                <h3>Email</h3>
-                <input type="email"/>
+                <h3>Email:</h3>
+                <input required name="email" onChange={changeForm} type="email"/>
             </div>
+            {errors.email && <div className="jello error">{errors.email}</div>}
             <div className="row">
-                <h3>Password</h3>
-                <input type="password"/>
+                <h3>Password:</h3>
+                <input required name="password" onChange={changeForm} type="password"/>
             </div>
+            {errors.password && <div className="jello error">{errors.password}</div>}
             <div className="btns">
-                <button>Sign Up</button>
-                <button><Link to={'/'}>Login</Link></button>
+                <button onClick={signUpUser}>Sign Up</button>
+                <button><Link to={'/'}>Sign In</Link></button>
             </div>
+            </form>
         </div>
+        {
+          showSpinner&&<Spinner/>
+        }
     </div>
   )
 }
