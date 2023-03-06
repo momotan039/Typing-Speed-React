50fc55127b925616407e82a7c189b2bb9b5e7c63
MOHAMMED <momotaha039@gmail.com>
2023-03-06 20:08:52 +0200

    create my confetti component

---
+++ src/components/My Confetti/MyConfetti.jsx
@@ -0,0 +1,12 @@
+import Confetti from 'react-confetti'
+
+export default function MyConfetti() {
+  return (
+    <div className="layer">
+        <Confetti/>
+        <h1>Congratulations!!</h1>
+        <h3>Your typing speed incresed to 80WPM</h3>
+        <button>Complete Joy</button>
+    </div>
+  )
+}
