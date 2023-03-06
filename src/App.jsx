d13ed22460a870d1abc1e8443cb586e1ab122319
momotan <momotaha039@gmail.com>
2023-03-06 16:06:38 +0200

    remove error user and add signup function to app context

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { createContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Paragraph } from "./components/Paragraph/Paragraph";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, useNavigate } from "react-router-dom";
 import Root from "./components/Root";
 import Settings from "./components/settings/Settings";
-import User, { LogIn, LogOut } from "./components/Utils/User.mjs";
+import { LogIn, LogOut, createAccount } from "./components/Utils/User.mjs";
 import Login from "./components/Login/Login";
 import SignUp from "./components/Signup/SignUp";
+import { getUserFromSorage } from "./components/Utils/LocalStorage.mjs";
 
 
 export const AppContext=createContext()
 function App() {
-  const [user,setUser]=useState(new User()),
-  const [errorUser,setErrorUser]=useState('')
+  const [user,setUser]=useState(getUserFromSorage())
   const generateChildrenRoute=()=>{
     const userChildren=[
       {
@@ -52,7 +52,8 @@ function App() {
   return <AppContext.Provider value={{
     user:user,
     logout:()=>LogOut(setUser),
-    login:(user)=>LogIn(user,setUser,setErrorUser)
+    login:(user)=>LogIn(user,setUser),
+    signUp:(user)=>createAccount(user)
   }}>
     <RouterProvider router={router}/>
   </AppContext.Provider>
