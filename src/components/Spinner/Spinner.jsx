c854088d32ec96e306e48ed79b55a69bc4d3a8c4
momotan <momotaha039@gmail.com>
2023-03-06 16:00:09 +0200

    add spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Spinner.css'
+export default function Spinner() {
+    return (
+        <div className="layer">
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
