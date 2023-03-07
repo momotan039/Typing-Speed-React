c75d1bd66c222b2db875c1eadcc3704b09d9218e
momotan <momotaha039@gmail.com>
2023-03-07 11:11:45 +0200

    edit path music

--- src/components/Utils/Sounds.mjs
+++ src/components/Utils/Sounds.mjs
@@ -2,7 +2,7 @@
 export const playCorrectSound=(enabledSound)=>{
     if(!enabledSound)
     return
-    const audio=new Audio('/src/assets/sounds/type1.mp3')
+    const audio=new Audio('/sounds/type1.mp3')
     audio.volume=0.3
     audio.play()
 }
@@ -10,7 +10,7 @@ export const playCorrectSound=(enabledSound)=>{
 export const playWrongSound=(enabledSound)=>{
     if(!enabledSound)
     return
-    const audio=new Audio('/src/assets/sounds/error.wav')
+    const audio=new Audio('/sounds/error.wav')
     audio.volume=0.3
     audio.play()
 }
@@ -18,7 +18,7 @@ export const playWrongSound=(enabledSound)=>{
 export const playConfettiSound=(enabledSound)=>{
     if(!enabledSound)
     return
-    const audio=new Audio('/src/assets/sounds/confetti2.mp3')
+    const audio=new Audio('/sounds/confetti2.mp3')
     // audio.volume=0.3
     audio.play()
 }
